1e45a42e8807379cdaa42170e82d4c38aeded0cf
zachw6 <zawilliams2@wisc.edu>
2021-01-01 17:13:28 -0600

    Updated things to work with Heroku.

--- server/server.js
+++ server/server.js
@@ -42,9 +42,9 @@ if (process.env.NODE_ENV === "production") {
   app.disable("x-powered-by");
   app.use(compression());
   app.use(morgan("common"));
-  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "..", "client", "build","index.html"));
   });
 }
 
